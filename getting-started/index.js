a12ca3716a70759f12bf809df69e01749261bb52
Marlonego <negoscapo@gmail.com>
2019-08-13 10:05:25 +0200

    added a sentence

--- getting-started/index.js
+++ getting-started/index.js
@@ -10,3 +10,5 @@ let city = "Spain"
 let  job = "capoeira Teacher"
 let hobby = "Sports"
 console.log(name, nickName, city, job, hobby,);
+
+console.log("John Smith is a 43 year old teacher" + "who lives in Berlin")
