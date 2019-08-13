acd980bc4c36061331b5ffeac82c445b3aa9d212
Marlonego <negoscapo@gmail.com>
2019-08-13 09:56:19 +0200

    exesise 2 done

--- getting-started/index.js
+++ getting-started/index.js
@@ -4,3 +4,9 @@ var lastName
 var lastName = "Torriente";
 
 console.log(firstName,lastName);
+let name = "Bala"
+let nickName = "Capoeira"
+let city = "Spain"
+let  job = "capoeira Teacher"
+let hobby = "Sports"
+console.log(name, nickName, city, job, hobby,);
