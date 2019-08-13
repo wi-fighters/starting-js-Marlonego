b208bb4af322737635a4fe09d0b2f09826acbfd6
Marlonego <negoscapo@gmail.com>
2019-08-13 15:59:55 +0200

    Finish the ex 4

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -17,6 +17,15 @@ const averageHour = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 +7 + 4;
 const scoreTest = 85 * 6 - (75+70+85+90+100);
 console.log(scoreTest);
 
+// 4.ex
+ 
+const firstAverage = 78 * 8;
+console.log(firstAverage);
+ const totalPoints = 80*9;
+ console.log(totalPoints);
+
+const minResult = totalPoints - firstAverage;
+console.log(`james needs ${minResult} to get an average of 80%`);
 
 
 
