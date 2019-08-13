ba67788c6d2da33cc1fb45f2dc063a0b81ea5166
Marlonego <negoscapo@gmail.com>
2019-08-13 15:16:13 +0200

    Add comments and finish the ex 3

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -1,3 +1,4 @@
+// 1.ex
 
 const x = "17";
 const y = "30";
@@ -5,9 +6,20 @@ const result = x / y * 100;
 
 console.log (result);
 
+// 2.ex
+
 const averageHour = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 +7 + 4;
  const otherResult = averageHour / 10;
  console.log(otherResult);
-8, 6, 5, 9, 8, 2, 1, 8.5, 7, 4
+
+//  3.ex
+
+const scoreTest = 85 * 6 - (75+70+85+90+100);
+console.log(scoreTest);
+
+
+
+
+
 
 
