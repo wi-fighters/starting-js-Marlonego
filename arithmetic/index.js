082224dbaa73d25781fd69e24d66158453d39a30
Marlonego <negoscapo@gmail.com>
2019-08-13 14:53:17 +0200

    First exersise corrected and second one done

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -1,4 +1,13 @@
-let x = "17";
-let y = "30";
-let result = x / y;
-console.log(result);
+
+const x = "17";
+const y = "30";
+const result = x / y * 100;
+
+console.log (result);
+
+const averageHour = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 +7 + 4;
+ const otherResult = averageHour / 10;
+ console.log(otherResult);
+8, 6, 5, 9, 8, 2, 1, 8.5, 7, 4
+
+
