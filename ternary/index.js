1738eaac8de1ddeec947638e86453fb1c770f122
Marlonego <negoscapo@gmail.com>
2019-08-14 10:16:25 +0200

    exersise 2 done

--- ternary/index.js
+++ ternary/index.js
@@ -1,4 +1,14 @@
+// 1 ex
+
 const legalAge = 18;
 const personAge = 34;
 const canDrive = (personAge >= legalAge) ? "Yes, you can drive! Hope you have a license" : "Nope, you're too young!";
 console.log(canDrive);
+
+// 2 ex
+
+const legalSpeed = 130;
+const currentSpeed =126;
+const speedCheck = (currentSpeed <= legalAge) ? "You're going too fast - slow down!" : "You're driving below the speed limit";
+console.log(speedCheck);
+
