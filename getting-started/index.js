0df18af884c1fb48d86acd0cf011a2a1296257c8
Marlonego <negoscapo@gmail.com>
2019-08-14 22:41:24 +0200

    ex 4 done and added some extra comments

--- getting-started/index.js
+++ getting-started/index.js
@@ -1,9 +1,13 @@
+// 1 ex 
+
 var firstName 
 var firstName = "Marlon";
-var lastName 
-var lastName = "Torriente";
+var familyName 
+var familyName = "Torriente";
+console.log(firstName,familyName);
 
-console.log(firstName,lastName);
+// 2 ex
+
 let name = "Bala";
 let nickName = "Capoeira";
 let city = "Spain"
@@ -11,8 +15,23 @@ let  job = "capoeira Teacher";
 let hobby = "Sports";
 console.log(name, nickName, city, job, hobby,);
 
+// 3 ex
+
 let name2 = "John Smith";
 let age = "is a 43 years old";
 let job2 = "teacher";
 let city2 = "who live in berlin";
  console.log(name2, age, job2, city2);
+
+//  4 ex
+
+let length = 16.78; 
+let lastName = "Johnson"; 
+let car = ""; 
+let isOpen = true;
+console.log(typeof(length));
+console.log(typeof(lastName));
+console.log(typeof(car));
+console.log(typeof(isOpen));
+
+
