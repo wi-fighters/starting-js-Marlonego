f2d93f3d034b70824f017be09e4c9a4a1e0d4dbc
Marlonego <negoscapo@gmail.com>
2019-08-14 10:30:04 +0200

    ex 3 done

--- ternary/index.js
+++ ternary/index.js
@@ -1,8 +1,8 @@
 // 1 ex
 
 const legalAge = 18;
-const personAge = 34;
-const canDrive = (personAge >= legalAge) ? "Yes, you can drive! Hope you have a license" : "Nope, you're too young!";
+const guyAge = 34;
+const canDrive = (guyAge >= legalAge) ? "Yes, you can drive! Hope you have a license" : "Nope, you're too young!";
 console.log(canDrive);
 
 // 2 ex
@@ -12,3 +12,9 @@ const currentSpeed =126;
 const speedCheck = (currentSpeed <= legalAge) ? "You're going too fast - slow down!" : "You're driving below the speed limit";
 console.log(speedCheck);
 
+//  3 ex
+
+const person = 26;
+const age = 21;
+const personAge = (person <= age) ? "Here's some juice" : "Here's some wine";
+console.log(personAge);
